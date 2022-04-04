76f431089c68c2b417cb099eab84bd821c4fb48b
unknown <33046@ma-web.nl>
2022-04-04 10:32:47 +0200

    sidebar

---
+++ js/script.js
@@ -0,0 +1,12 @@
+const body = document.querySelector('body'),
+      sidebar = body.querySelector('nav'),
+      toggle = body.querySelector(".toggle"),
+      modeText = body.querySelector(".mode-text"),
+      Time;
+      
+
+
+toggle.addEventListener("click" , () =>{
+    sidebar.classList.toggle("close");
+})
+
