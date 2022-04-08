2d796ad6e15ee7896073057266471ab32d15185d
judensio2004 <3433@ma-web.nl>
2022-04-08 13:30:00 +0200

    pull

--- js/script.js
+++ js/script.js
@@ -5,8 +5,12 @@ const body = document.querySelector('body'),
 
 
 
+
+
+let uitgeklapt = false;
 toggle.addEventListener("click" , () =>{
     sidebar.classList.toggle("close");
+    uitgeklapt = !uitgeklapt;
 })
 
 const updatetime = function(){
@@ -15,13 +19,78 @@ const updatetime = function(){
     var n = date.toDateString();
     
     var time = date.toLocaleTimeString();
-    
-    document.getElementById('js--timeanddate').innerHTML = n + ' ' + time;
+
+    console.log('date:', n);
 
 
-    setTimeout(updatetime, 1000)
+    if(uitgeklapt === true){
+        document.getElementById('js--timeanddate').innerHTML = n + ' ' + time;
+
+    }
+    if(uitgeklapt === false){
+        document.getElementById('js--timeanddate').innerHTML = n + ' ' + time;
+    }
+
+    setTimeout(updatetime, 1)
+
 }
+
 
 updatetime()
 
 
+// dataAPI code
+
+"use strict"
+const timeDelay = 10000; // time delay refresh data
+let refreshTimer = window.setInterval(renderData, timeDelay); // timer data opvragen van server
+
+// jouw persoonlijke URL
+const mijnDataURL = "https://data.softwaredeveloper.amsterdam/api/device/71c87eb9/latest"; 
+
+const dataDiv = document.getElementById("dataDiv"); // hier komt de data
+
+async function getSensorData() {
+    let url = mijnDataURL;
+    try {
+        let response = await fetch(url);
+        return await response.json();
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+async function renderData() {
+    let measurementRaw = await getSensorData();
+    let measurement = measurementRaw.data[0];
+    console.log(measurement);
+
+     // Nieuwe Javascript date maken met de de datum en tijd van de meting
+     let datum =  new Date(measurement.datum);
+   
+     // De meting gegevens wegschrijven naar de div                
+     dataDiv.innerHTML = `<p>Sensor: ${measurement.sensor} 
+        </p><p>Temperature: ${measurement.value1}
+        </p><p> Humidity: ${measurement.value2}
+        </p><p> Heatindex: ${measurement.value3}</p>`;
+}
+
+renderData(); // start immediately
+
+document.querySelector('#sidebar').addEventListener('wheel', preventScroll, {passive: false});
+
+function preventScroll(e){
+    e.preventDefault();
+    e.stopPropagation();
+
+    return false;
+}
+
+document.querySelector('#js--timeanddate').addEventListener('wheel', preventScroll, {passive: false});
+
+function preventScroll(e){
+    e.preventDefault();
+    e.stopPropagation();
+
+    return false;
+}
